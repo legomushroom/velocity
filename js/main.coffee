1c80ead9471a8c3c77d0e6d4690376a1199d0213
sol0mka <legomushroom@gmail.com>
2014-05-14 11:59:26 +0300

    working on fast

--- js/main.coffee
+++ js/main.coffee
@@ -1,12 +1,36 @@
 class Main
   constructor:(@o={})->
     @vars()
-    new window.Cross
+    @init()
 
   vars:->
+    @$fast = $('#js-fast')
+    @$car1 = $('#js-car1')
+    @$car2 = $('#js-car2')
+
+  init:->
+    @s = 1
+    @start = 0*@s
+    @dur   = 400*@s
+
+    @$car1
+      .velocity { right: '-20%', width: '0%', opacity: 1, duration: @dur }
+    @start = 0*@s
+    @dur   = 4500*@s
+    for child, i in @$fast.children()
+      $child = $ child
+      $child
+        .velocity({ rotateZ: 40 }, {
+          (delay: 200+helpers.rand(0,300))*@s, duration: 100
+        }).velocity({ rotateZ: 0 },  {
+          delay: (60+helpers.rand(0,50))*@s, duration: @dur, easing: 'quake'
+        })
+
+    # @$car1
+    #   .velocity { right: '-20%', width: '0%', opacity: 1 }
 
 setTimeout ->
-  new window.Cross
+  new Main
 , 1000
 
 # setTimeout ->
--- js/main.js
+++ js/main.js
@@ -5,17 +5,55 @@
     function Main(o) {
       this.o = o != null ? o : {};
       this.vars();
-      new window.Cross;
+      this.init();
     }
 
-    Main.prototype.vars = function() {};
+    Main.prototype.vars = function() {
+      this.$fast = $('#js-fast');
+      this.$car1 = $('#js-car1');
+      return this.$car2 = $('#js-car2');
+    };
+
+    Main.prototype.init = function() {
+      var $child, child, i, _i, _len, _ref, _results;
+      this.s = 1;
+      this.start = 0 * this.s;
+      this.dur = 400 * this.s;
+      this.$car1.velocity({
+        right: '-20%',
+        width: '0%',
+        opacity: 1,
+        duration: this.dur
+      });
+      this.start = 0 * this.s;
+      this.dur = 4500 * this.s;
+      _ref = this.$fast.children();
+      _results = [];
+      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+        child = _ref[i];
+        $child = $(child);
+        _results.push($child.velocity({
+          rotateZ: 40
+        }, {
+          delay: 200 * this.s,
+          duration: 100
+        }).velocity({
+          rotateZ: 0
+        }, {
+          delay: 60 + helpers.rand(0, 50),
+          duration: this.dur,
+          easing: 'quake'
+        }));
+      }
+      return _results;
+    };
 
     return Main;
 
   })();
 
   setTimeout(function() {
-    return new window.Cross;
+    return new Main;
   }, 1000);
 
 }).call(this);
